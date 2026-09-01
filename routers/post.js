6998662265c32743d47fb4817aa06b8c3c27f3e2
francesco-guttuso <francescoguttuso@gmail.com>
2026-09-02 00:30:01 +0200

    Add POST route for creating a new post

--- routers/post.js
+++ routers/post.js
@@ -9,3 +9,7 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   res.send(`Visualizzazione del post ${req.params.id}`);
 });
+
+router.post("/", (req, res) => {
+  res.send("Creazione di un nuovo post");
+});
